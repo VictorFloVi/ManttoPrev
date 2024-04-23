d3bf836b9fac3be6da485f9af3554396c58e291b
VictorFlow <victorflovi@gmail.com>
2024-04-23 05:38:01 -0500

    Creación de Login y Registro de Usuario, en estado nativo

--- app/src/main/java/com/example/manttoprev/MainActivity.java
+++ app/src/main/java/com/example/manttoprev/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.manttoprev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.manttoprev.Vista.Login;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnIngresarCredenciales;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIngresarCredenciales=findViewById(R.id.btnIngresarCredenciales);
+
+        btnIngresarCredenciales.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+        });
     }
 }
